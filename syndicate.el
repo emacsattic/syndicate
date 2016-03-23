c74d7178137a0b69abe4b0c86cebfd99a30a927b
Kawin Nikomborirak <kawinnikom@gmail.com>
2016-03-23 12:34:27 -0700

    header license + copyright

--- syndicate.el
+++ syndicate.el
@@ -1,8 +1,28 @@
 ;;; syndicate.el --- evil keybindings for org-mode
 
+;; Copyright (c) 2016 Kawin Nikomborirak
+
+;; Author: Kawin Nikomborirak
 ;; URL: https://github.com/KNX32542/syndicate.git
 ;; Keywords: evil org bindings
 ;; Package-Requires: ((evil "1.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; Still in early stages, I plan to use and develop this for a while
